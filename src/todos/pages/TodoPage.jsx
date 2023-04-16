ab13bc69f31064a8af50de4139ccf4a0dac10cbd
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-16 16:36:55 +0200

    adding add button

--- src/todos/pages/TodoPage.jsx
+++ src/todos/pages/TodoPage.jsx
@@ -1,12 +1,28 @@
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
 import { TodoLayout } from "../layout/TodoLayout";
-import { TaskView } from "../views";
+import { NothingSelectedView, TaskView } from "../views";
 
 
 export const TodoPage = () => {
     return (
         <TodoLayout>
-            {/* <NothingSelectedView /> */}
-            <TaskView />
+            <NothingSelectedView />
+            {/* <TaskView /> */}
+
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'secondary.main',
+                    ':hover': { backgroundColor: 'secondary.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50,
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }}/>
+            </IconButton>
         </TodoLayout>
     );
 }
