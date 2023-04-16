372d3cb42ee166c2a220d2cf1981ea3a694b079c
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-16 13:01:32 +0200

    styling register page with layout technique

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,13 +1,64 @@
-import { Typography } from "@mui/material";
+import { Link as RouterLink } from 'react-router-dom';
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayaout } from '../layout/AuthLayaout';
 
 
 export const RegisterPage = () => {
 
     return (
-        <>
-            <Typography>Register page</Typography>
-        </>
+        <AuthLayaout title='Sign Up'>
+            <form>
+                <Grid container>
+                    <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                        <TextField 
+                            label="Name" 
+                            type="text" 
+                            placeholder="Your name"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                        <TextField 
+                            label="Email" 
+                            type="email" 
+                            placeholder="example@email.com"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                        <TextField 
+                            label="Password" 
+                            type="password" 
+                            placeholder="Password"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid container
+                        spacing={ 2 }
+                        sx={{ mb: 2, mt: 1 }}
+                    >
+                        <Grid item xs={ 12 }>
+                            <Button variant="contained" fullWidth>
+                                Create Account
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    <Grid container
+                        direction="row"
+                        justifyContent="end"
+                    >
+                        <Typography sx={{ mr: 1  }}>Already have a TodoWeb account?</Typography>
+                        <Link component={ RouterLink } to="/auth/login">
+                            Sign In
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </AuthLayaout>
     );
 }
+
+
 
 
