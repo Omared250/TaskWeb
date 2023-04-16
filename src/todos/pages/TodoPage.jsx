882c79f3843ebd14af855b5bb57140a1d0114356
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-16 15:50:26 +0200

    nothing view style

--- src/todos/pages/TodoPage.jsx
+++ src/todos/pages/TodoPage.jsx
@@ -1,12 +1,13 @@
 import { Typography } from "@mui/material";
 import { TodoLayout } from "../layout/TodoLayout";
+import { NothinSelectedView } from "../views/NothinSelectedView";
 
 
 export const TodoPage = () => {
     return (
         <TodoLayout>
-            <Typography>Duis quis veniam ipsum sint elit velit sit.</Typography>
-            {/* NothinSelected */}
+            {/* <Typography>Duis quis veniam ipsum sint elit velit sit.</Typography> */}
+            <NothinSelectedView />
             {/* NoteView */}
         </TodoLayout>
     );
