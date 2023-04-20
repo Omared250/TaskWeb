ccd0e76b24ffdbb405c505bf67518c7d7d6e0952
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-20 12:19:32 +0200

    adding data picker

--- src/todos/components/CalendarModal.jsx
+++ src/todos/components/CalendarModal.jsx
@@ -1,5 +1,8 @@
+import { addHours } from "date-fns";
 import { useState } from "react";
 import Modal from "react-modal";
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
 
 const customStyles = {
     content: {
@@ -16,7 +19,28 @@ Modal.setAppElement('#root');
 
 export const CalendarModal = () => {
 
-    const [isOpen, setIsOpen] = useState( true )
+    const [isOpen, setIsOpen] = useState( true );
+
+    const [formValues, setFormValues] = useState({
+        title: 'Omar',
+        notes: 'Ascanio',
+        start: new Date(),
+        end: addHours(new Date(), 2),
+    });
+
+    const onInputChange = ({ target }) => {
+        setFormValues({
+            ...formValues,
+            [target.name] : target.value,
+        })
+    };
+
+    const onDateChanged = ( event, changing ) => {
+        setFormValues({
+            ...formValues,
+            [changing]: event
+        });
+    };
 
     const onCloseModal = () => {
         console.log('cerrando modal');
@@ -32,42 +56,57 @@ export const CalendarModal = () => {
             overlayClassName="modal-fondo"
             closeTimeoutMS={ 200 }
         >
-            <h1> Nuevo evento </h1>
+            <h1> New Event </h1>
             <hr />
             <form className="container">
 
                 <div className="form-group mb-2">
-                    <label>Fecha y hora inicio</label>
-                    <input className="form-control" placeholder="Fecha inicio" />
+                    <label>Start date and time</label>
+                    <DatePicker 
+                        selected={ formValues.start }
+                        onChange={( event ) => { onDateChanged( event, 'start' ) }}
+                        className="form-control"
+                        dateFormat="Pp"
+                    />
                 </div>
 
                 <div className="form-group mb-2">
-                    <label>Fecha y hora fin</label>
-                    <input className="form-control" placeholder="Fecha inicio" />
+                    <label>End date and time</label>
+                    <DatePicker
+                        minDate={ formValues.start }
+                        selected={ formValues.end }
+                        onChange={( event ) => { onDateChanged( event, 'end' ) }}
+                        className="form-control"
+                        dateFormat="Pp"
+                    />
                 </div>
 
                 <hr />
                 <div className="form-group mb-2">
-                    <label>Titulo y notas</label>
+                    <label>Title and Notes</label>
                     <input 
                         type="text" 
                         className="form-control"
-                        placeholder="Título del evento"
+                        placeholder="Event title"
                         name="title"
                         autoComplete="off"
+                        value={ formValues.title }
+                        onChange={ onInputChange }
                     />
-                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+                    <small id="emailHelp" className="form-text text-muted">Short Description</small>
                 </div>
 
                 <div className="form-group mb-2">
                     <textarea 
                         type="text" 
                         className="form-control"
-                        placeholder="Notas"
+                        placeholder="Notes"
                         rows="5"
                         name="notes"
+                        value={ formValues.notes }
+                        onChange={ onInputChange }
                     ></textarea>
-                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+                    <small id="emailHelp" className="form-text text-muted">Additional Information</small>
                 </div>
 
                 <button
@@ -75,7 +114,7 @@ export const CalendarModal = () => {
                     className="btn btn-outline-primary btn-block"
                 >
                     <i className="far fa-save"></i>
-                    <span> Guardar</span>
+                    <span>Save</span>
                 </button>
 
             </form>
