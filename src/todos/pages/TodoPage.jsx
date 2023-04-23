984c021c0d9c8dbb4b7f19d22585d1f6f345e852
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-23 19:49:38 +0200

    updating UI

--- src/todos/pages/TodoPage.jsx
+++ src/todos/pages/TodoPage.jsx
@@ -1,15 +1,10 @@
-import { IconButton } from "@mui/material";
-import { AddOutlined } from "@mui/icons-material";
 import { TodoLayout } from "../layout/TodoLayout";
-import { NothingSelectedView, TaskView } from "../views";
 import { CalendarView } from "../views/CalendarView";
 
 
 export const TodoPage = () => {
     return (
         <TodoLayout>
-            {/* <NothingSelectedView /> */}
-            {/* <TaskView /> */}
             <CalendarView />
         </TodoLayout>
     );
--- src/todos/views/index.js
+++
@@ -1,3 +0,0 @@
-
-export * from './NothingSelectedView';
-export * from './TaskView';
