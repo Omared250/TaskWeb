f03b8792b585a9d3013a9d9a65f8074b2a5b5e3c
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-23 18:28:16 +0200

    basis to create an event

--- src/todos/components/FloattingActionButton.jsx
+++ src/todos/components/FloattingActionButton.jsx
@@ -1,7 +1,27 @@
 import { AddOutlined } from "@mui/icons-material"
 import { IconButton } from "@mui/material"
+import { useCalendarStore, useUiStore } from "../../hooks"
+import { addHours } from "date-fns";
 
 export const FloattingActionButton = () => {
+
+    const { openDateModal } = useUiStore();
+    const { setActiveEvent } = useCalendarStore();
+
+    const handleClickNew = () => {
+        setActiveEvent({
+            title: '',
+            notes: '',
+            start: new Date(),
+            end: addHours( new Date(), 2 ),
+            bgColor: '#fafafa',
+            user: {
+                _id: '123',
+                name: 'Omar'
+            }
+        });
+        openDateModal();
+    };
 
     return (
         <IconButton
@@ -14,6 +34,7 @@ export const FloattingActionButton = () => {
                 right: 50,
                 bottom: 50,
             }}
+            onClick={ handleClickNew }
         >
         <AddOutlined sx={{ fontSize: 30 }}/>
     </IconButton>
