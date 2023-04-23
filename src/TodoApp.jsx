4de82998c450130fa69a3f250a215eb4a9bbb6ab
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-23 13:15:52 +0200

    adding store to the project using redux

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./router/AppRouter";
 import { AppTheme } from "./theme/AppTheme";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function TodoApp() {
 
     return (
-        <>
+        <Provider store={ store }>
             <BrowserRouter>
                 <AppTheme>
                     <AppRouter />
                 </AppTheme>
             </BrowserRouter>
-        </>
+        </Provider>
     );
 
 }
