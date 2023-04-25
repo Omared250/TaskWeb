73a425bf5e677cd560e87782d542bae83c536b97
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-25 12:35:40 +0200

    adding auth to the store

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { TodosRoutes } from "../todos/routes/TodosRoutes";
-import { getEnvVariables } from "../helpers";
-
 
 export const AppRouter = () => {
-
-    console.log(getEnvVariables());
-
     return (
         <Routes>
             <Route path="/auth/*" element={ <AuthRoutes /> } />
