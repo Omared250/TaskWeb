af9535d1dcbbd06c7b2177bf92192ae8929cc3d2
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-04-25 12:25:00 +0200

    adding env variables

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { TodosRoutes } from "../todos/routes/TodosRoutes";
-import { TodoPage } from "../todos/pages";
+import { getEnvVariables } from "../helpers";
 
 
 export const AppRouter = () => {
+
+    console.log(getEnvVariables());
+
     return (
         <Routes>
             <Route path="/auth/*" element={ <AuthRoutes /> } />
